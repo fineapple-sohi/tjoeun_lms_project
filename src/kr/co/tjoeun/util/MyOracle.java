94ec16ef980824f8caedce845aca613194bb1993
tjoeunoff <tjoeunoff@gmail.com>
2021-09-30 18:57:14 +0900

    머지2

--- src/kr/co/tjoeun/util/MyOracle.java
+++ src/kr/co/tjoeun/util/MyOracle.java
@@ -1,13 +1,3 @@
 package kr.co.tjoeun.util;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
 
-public class MyOracle {
-	private static Connection conn;
-	private static String driver="oracle.jdbc.OracleDriver";
-	private static String url="jdbc:oracle:thin:@localhost:1521:xe";	//1521
-
-	
-}
