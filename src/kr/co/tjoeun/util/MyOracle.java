f05b59333eb2d4c0a93f08b84add4cba361df9b9
tjoeunoff <tjoeunoff@gmail.com>
2021-09-30 18:50:48 +0900

    머지하지ㅏ마세요

--- src/kr/co/tjoeun/util/MyOracle.java
+++ src/kr/co/tjoeun/util/MyOracle.java
@@ -8,24 +8,6 @@ public class MyOracle {
 	private static Connection conn;
 	private static String driver="oracle.jdbc.OracleDriver";
 	private static String url="jdbc:oracle:thin:@localhost:1521:xe";	//1521
-	private static String user="scott";	//scott
-	private static String password="tiger";	//tiger
-	
-	public MyOracle() {}
-	
-	public static Connection getConnection() throws SQLException {
-		
-		if(conn == null || conn.isClosed()) {
-			try {
-				Class.forName(driver);
-			} catch (ClassNotFoundException e) {
-				e.printStackTrace();
-			}
-			conn=DriverManager.getConnection(url, user, password);
-		}	
-		return conn;
-		
-	}
-	
+
 	
 }
